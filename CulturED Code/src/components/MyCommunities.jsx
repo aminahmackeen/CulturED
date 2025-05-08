486cdf3f81b1dde28456ecdfe296b4a619c2590b
emma-esteboss <esteb@uw.edu>
2025-05-07 21:50:42 -0700

    MyCommunities.jsx, schema, realtime db test

--- CulturED Code/src/components/MyCommunities.jsx
+++ CulturED Code/src/components/MyCommunities.jsx
@@ -1,6 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, set as firebaseSet, 
+push as firebasePush, onValue } from 'firebase/database';
+
+// what does useEffect do
+// making the initial object in state
+
+
 
 function MyCommunities() {
+  
+  const addCommunity = () => {
+    const newCommunityObj = {
+      "firbaseKey": "",
+      "owner": userObj.userEmail,
+      "name": "",
+      "users": []
+    }
+  }
+  
+  // reading to database
+  useEffect(() => {
+    // subscribe to the database
+    const db = getDatabase();
+    const communitiesRef = ref(db, "communities") // address
+    
+    // takes in a callback function. Similar to: addEventListener('firebase value change', function)
+    // this downloads a snapshot (a firebase object) of the data anytime it changes
+    onValue(communitiesRef, (snapshot) => {
+      // console.log("database changed");
+      const dataObj = snapshot.val();
+      console.log(dataObj); // is an {}
+
+      const keyArray = Object.keys(dataObj); // keyArray is ['','','']
+      const dataArray = keyArray.map((keyString) => {
+        const transformed = dataObj[keyString]; // get value at that key
+        transformed.firebaseKey = keyString;
+        return transformed; // put into new array
+      })
+      setCommunityStateArray(dataArray); // needs to be an [{},{},{}]
+
+    });
+  
+  }, [])
+
+// const updateCommunityArray = [...msgStateArray, newMessageObj];
+const db = getDatabase();
+// const communityRef = ref(db, "community");
+// firebaseSet(communityRef, newCommunityObj); // setting indv obj
+
+// writing to database
+const communitiesRef = ref(db,"communities");
+firebasePush(communitiesRef, newCommunityObj) // push into an object with a key like an array
 
 }
 
--- CulturED Code/src/components/ShareStory.jsx
+++ CulturED Code/src/components/ShareStory.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ref, push } from 'firebase/database';
-import { db } from '../firebase';
+import { db } from '../firebase';''
 
 function ShareStory() {
   const [title, setTitle] = useState('');
